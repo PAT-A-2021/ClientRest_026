95797bc269acfd013fcb3b18cf9d699d4f3a8f54
muhammadrisqullah <80031539+muhammadrisqullah@users.noreply.github.com>
2022-01-03 21:19:25 +0700

    Add project files.

---
+++ ClientRest_20190140026_Muhammad Risqullah Zarvino/ClassData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+using static ClientRest_20190140026_Muhammad_Risqullah_Zarvino.Program;
+
+namespace ClientRest_20190140026_Muhammad_Risqullah_Zarvino
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach(var mhs in data)
+            {
+                Console.WriteLine("Nama :" + mhs.nama);
+                Console.WriteLine("Nim :" + mhs.nim);
+                Console.WriteLine("Prodi :" + mhs.prodi);
+                Console.WriteLine("Angkatan :" + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
